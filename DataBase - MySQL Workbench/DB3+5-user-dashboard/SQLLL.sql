9d212e74e08481006a90c6c57d5491692744134b
Mohammad-Al-Turk <mohammad.al.turk.mt@gmail.com>
2024-12-27 23:58:07 +0200

    3rd and 5th DB Task

---
+++ DataBase - MySQL Workbench/DB3+5-user-dashboard/SQLLL.sql
@@ -0,0 +1,47 @@
+-- USE dashboard;
+-- SELECT * 
+-- FROM users;
+
+
+
+-- SELECT fname 
+-- FROM users;
+
+
+
+-- SELECT * 
+-- FROM users
+-- WHERE iduser = 1;
+
+-- SELECT * 
+-- FROM users
+-- WHERE iduser = 1 OR iduser = 2 ;
+
+-- SELECT * 
+-- FROM users
+-- WHERE iduser >1 ;
+
+
+
+-- INSERT INTO users (fname,lname,password,discription,Bio)
+-- VALUES('mohammad','Al-Turk','asdasdasd','hi','hello');
+
+
+
+-- INSERT INTO users (fname,lname,password,discription,Bio)
+-- VALUES('mo','Al-Turk','2665151','hi','hello');
+
+
+-- UPDATE users SET fname = 'turk', Bio='hi hi hi hi' WHERE iduser =2;
+
+
+-- DELETE FROM users WHERE  iduser =3;
+
+
+
+
+
+
+
+
+
